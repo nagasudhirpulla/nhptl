818dc012c180a20a94655437e3862d7a825a431d
NagaSudhir Pulla <nagasudhirpulla@gmail.com>
2016-11-10 06:28:38 +0530

    Update application-create.js

--- project/views/application-create.js
+++ project/views/application-create.js
@@ -25,11 +25,3 @@ function submitApplication() {
         }
     });
 }
-
-function getValObjFromDom(idArray) {
-    var valObj = {};
-    for (var i = 0; i < idArray.length; i++) {
-        valObj[idArray[i]] = document.getElementById(idArray[i]).value;
-    }
-    return valObj;
-}
